4a0c4f046da5afa08d726f398e3b69288b28bdce
Mr.Tian <954155714@qq.com>
2015-04-21 18:52:53 +0800

    修改一些显示信息

--- js/init.js
+++ js/init.js
@@ -1,80 +1,63 @@
 /*-----------------------------------------------------------------------------------
-/*
-/* Init JS
-/*
------------------------------------------------------------------------------------*/
+ /*
+ /* Init JS
+ /*
+ -----------------------------------------------------------------------------------*/
 
- jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
+    /*----------------------------------------------------*/
+    /*	Flexslider
+     /*----------------------------------------------------*/
+    //$('#intro-slider').flexslider({
+    //    animation: 'fade',
+    //    controlNav: false
+    //});
 
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
-   $('#intro-slider').flexslider({
-      animation: 'fade',
-      controlNav: false,
-   });
+    /*----------------------------------------------------*/
+    /*	contact form
+     ------------------------------------------------------*/
 
-/*----------------------------------------------------*/
-/*	gmaps
-------------------------------------------------------*/
+  /*  $('form#contactForm button.submit').click(function () {
 
-   var map;
+        $('#image-loader').fadeIn();
 
-   // main directions
-   map = new GMaps({
-      el: '#map', lat: 14.553533, lng: 121.052124, zoom: 14, zoomControl : true,
-      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
-   });
+        var contactName = $('#contactForm #contactName').val();
+        var contactEmail = $('#contactForm #contactEmail').val();
+        var contactSubject = $('#contactForm #contactSubject').val();
+        var contactMessage = $('#contactForm #contactMessage').val();
 
-   // add address markers
-   map.addMarker({ lat: 14.552891, lng: 121.051815, title: 'Eco Tower',
-   infoWindow: { content: '<p>You can add your address 1 here</p>' } });
+        var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+            '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
 
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
+        $.ajax({
 
-   $('form#contactForm button.submit').click(function() {
+            type: "POST",
+            url: "inc/sendEmail.php",
+            data: data,
+            success: function (msg) {
 
-      $('#image-loader').fadeIn();
+                // Message was sent
+                if (msg == 'OK') {
+                    $('#image-loader').fadeOut();
+                    $('#message-warning').hide();
+                    $('#contactForm').fadeOut();
+                    $('#message-success').fadeIn();
+                }
+                // There was an error
+                else {
+                    $('#image-loader').fadeOut();
+                    $('#message-warning').html(msg);
+                    $('#message-warning').fadeIn();
+                }
 
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
             }
 
-	      }
+        });
 
-      });
+        return false;
 
-      return false;
-
-   });
+    });*/
 
 
 });
